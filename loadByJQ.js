87cb6e38a4adac3e8159ef974baaa7f67b08a483
ZCTMDC <ZCTMDC@icloud.com>
2019-03-15 13:38:22 +0800

    简化代码，增加提示

--- loadByJQ.js
+++ loadByJQ.js
@@ -1,6 +1,6 @@
 /**
  *
- *动态加载js文件
+ * 动态加载js文件
  *
  * @param {string} url 需要加载的js文件url
  * @param {Function} callback 加载完回调函数
@@ -28,11 +28,8 @@ function loadScript(url, callback) {
 var jqCdnUrl = 'https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js';
 
 loadScript(jqCdnUrl, function () {
-  let fakeQQjsUrl =
-    'https://raw.githubusercontent.com/zctmdc/fakeQQInfo/master/fakeInfo.js';
-  $.get(fakeQQjsUrl,
-    function (data) {
-      $('head').append($("<script type='text/javascript'></script>").text(data));
-    }
-  );
+  //如果原网页含有JQ 只需要执行以下方法即可
+  $.get('https://raw.githubusercontent.com/zctmdc/fakeQQInfo/master/fakeInfo.js', function (data) {
+    $('head').append($("<script type='text/javascript'></script>").text(data));
+  });
 });
