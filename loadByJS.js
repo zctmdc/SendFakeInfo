06f9974a56b91b986f85935067fc761379ff24df
ZCTMDC <ZCTMDC@icloud.com>
2019-03-15 15:34:14 +0800

    增加兼容性

--- loadByJS.js
+++ loadByJS.js
@@ -1,6 +1,6 @@
 /**
+ * 加载js代码到网页
  *
- *  加载js代码到网页
  * @param {Text} data js代码文本
  */
 function loadScript(data) {
@@ -9,7 +9,29 @@ function loadScript(data) {
   script.text = data;
   document.head.appendChild(script);
 }
-
+/**
+ * 获取XMLHttpRequest对象，用于在后台与服务器交换数据   
+ *
+ * @returns
+ */
+function getxmlHttpRequestObject() {
+  let xmlHttp;
+  try {
+    xmlHttp = new XMLHttpRequest();
+  } catch (e) {
+    try {
+      xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {
+      try {
+        xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+      } catch (e) {
+        alert("您的浏览器不支持AJAX！");
+        return false;
+      }
+    }
+  }
+  return xmlHttp;
+}
 /**
  *  ajax get方法
  *
@@ -17,8 +39,7 @@ function loadScript(data) {
  * @param {Function} fnSucceed 成功回调方法
  */
 function ajaxget(url, fnSucceed) {
-  // XMLHttpRequest对象用于在后台与服务器交换数据   
-  let xhr = new XMLHttpRequest();
+  let xhr = getxmlHttpRequestObject();
   xhr.open('GET', url, true);
   xhr.onreadystatechange = function () {
     xhr.readyState == 4 && xhr.status == 200 && fnSucceed && fnSucceed(xhr.responseText);
